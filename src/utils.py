24917cb31689ec8a24c59cdfd534f219f6bc2c56
Jujar Singh <jujarsingh1511@gmail.com>
2026-04-01 21:50:09 +0530

    Improved function structure

--- src/utils.py
+++ src/utils.py
@@ -6,41 +6,71 @@ import numpy as np
 
 
 
-def calculate_mean(data):
+def calculate_mean(data: list[float]) -> float:
     """
     Calculate mean of a list of numbers.
 
-    :param data: List of Values (must be a non-empty list).
-    :return: float - mean(average) of the list.
+    Args:
+        data (list[float]): Input list of numbers.
+
+    Returns:
+        float: Mean value.
+
+    Raises:
+        ValueError: If list is empty.
+        Type error: If input is not list.
     """
-    if not isinstance(data, list) or len(data) == 0:
-        raise ValueError("Input must be a non-empty list")
+    if not isinstance(data, list):
+        raise TypeError("Input must be a list.")
+
+    if len(data) == 0:
+        raise ValueError("List cannot be empty.")
 
     return float(np.mean(data))
 
 
-def calculate_variance(data):
+def calculate_variance(data: list[float]) -> float:
     """
     Calculate variance of a list of numbers.
 
-    :param data: List of Values (must be a non-empty list).
-    :return: float - variance of the list.
+    Args:
+        data (list[float]): Input list of numbers.
+
+    Returns:
+        float: Variance of list of numbers.
+
+    Raises:
+        ValueError: If list is empty.
+        Type error: If input is not list.
     """
-    if not isinstance(data, list) or len(data) == 0:
-        raise ValueError("Input must be a non-empty list")
+    if not isinstance(data, list):
+        raise TypeError("Input must be a list.")
+
+    if len(data) == 0:
+        raise ValueError("List cannot be empty.")
 
     return float(np.var(data))
 
 
-def normalize_data(data):
+def normalize_data(data: list[float]) -> list[float]:
     """
     Normalize data using Min-Max scaling.
 
-    :param data: List of Values (must be a non-empty list).
-    :return: List of normalized data using Min-Max scaling.
+    Args:
+        data (list[float]): Input list of numbers.
+
+    Returns:
+        list: list of normalized data.
+
+    Raises:
+        ValueError: If list is empty or All values are the same.
+        Type error: If input is not list.
     """
-    if not isinstance(data, list) or len(data) == 0:
-        raise ValueError("Input must be a non-empty list")
+    if not isinstance(data, list):
+        raise TypeError("Input must be a list")
+
+    if len(data) == 0:
+        raise ValueError("List cannot be empty.")
 
     min_val = min(data)
     max_val = max(data)
